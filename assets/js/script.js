1982cf5d16b4ca9ff7732b1892bbd573d1ed2291
Nayaab Ali <www.nabi0300@gmail.com>
2023-08-11 15:35:46 -0700

    made portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const textToType = "Hello, world! This is a Typed.js example.";
+
+  const options = {
+    strings: ["Web Developer","Graphic Designer","Freelancer","Student"],
+    typeSpeed: 100,       
+    backSpeed: 60,           
+    loop: true,          
+  };
+
+  const typed = new Typed(".typing", options);
+});
+
+function updateProgressBar(percent) {
+    const progressBar = document.getElementById("my-progress-bar");
+    progressBar.style.width = percent + "%";
+  }
+  updateProgressBar(96);
+  
+
